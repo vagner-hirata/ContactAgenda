7d61a9af8169b89588c5da21e5aa6cf185291a76
Vagner Hirata <vagner.hirata@gmail.com>
2025-02-14 17:39:26 -0300

    Basic project structure

---
+++ src/main/java/com/vh/ContactAgenda/ContactAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.vh.ContactAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAgendaApplication.class, args);
+	}
+
+}
